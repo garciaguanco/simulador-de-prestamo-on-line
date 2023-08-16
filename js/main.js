1a39dbb41bfbe02f1eac4f76c3553aae382a667a
sbeastian garcia <garciaguanco@icloud.com>
2023-08-16 18:48:01 -0300

    modifico variable en alert

--- js/main.js
+++ js/main.js
@@ -16,17 +16,16 @@ const prestamo = {
     pagoMensual: 0
 };
 
-
 // Pedir al usuario los datos del préstamo y validar que sean números válidos
-const montoPrestamo = parseFloat(prompt("Ingrese el monto del préstamo:"));
-const tasaInteresAnual = parseFloat(prompt("Ingrese la tasa de interés anual (%):"));
-const plazoMeses = parseInt(prompt("Ingrese el plazo del préstamo en meses:"));
+prestamo.monto = parseFloat(prompt("Ingrese el monto del préstamo:"));
+prestamo.tasa = parseFloat(prompt("Ingrese la tasa de interés anual (%):"));
+prestamo.plazo = parseInt(prompt("Ingrese el plazo del préstamo en meses:"));
 
 // Validar que los datos ingresados  y generar mensajes de validación
 const mensajesValidacion = [
-    { condicion: isNaN(prestamo.monto) || prestamo.monto <= 0, mensaje: "Por favor, ingrese un monto válido mayor que cero." },
-    { condicion: isNaN(prestamo.tasa) || prestamo.tasa <= 0, mensaje: "Por favor, ingrese una tasa de interés válida mayor que cero." },
-    { condicion: isNaN(prestamo.plazo) || prestamo.plazo <= 0, mensaje: "Por favor, ingrese un plazo válido mayor que cero." }
+    { condicion: isNaN(prestamo.monto) || prestamo.monto <= 0, mensaje: "Por favor, ingrese un monto válido mayor que cero. Error en monto" },
+    { condicion: isNaN(prestamo.tasa) || prestamo.tasa <= 0, mensaje: "Por favor, ingrese una tasa de interés válida mayor que cero. Error en tasa" },
+    { condicion: isNaN(prestamo.plazo) || prestamo.plazo <= 0, mensaje: "Por favor, ingrese un plazo válido mayor que cero. Error en plazo" }
 ];
 
 // Filtrar mensajes de validación que sean verdaderos y mostrarlos
@@ -35,24 +34,20 @@ if (mensajesMostrar.length > 0) {
     mensajesMostrar.forEach(mensaje => console.log(mensaje));
 } else {
 
-
-
 // Calcular el pago mensual usando la función
 prestamo.pagoMensual = calcularPagoMensual(prestamo.monto, prestamo.tasa, prestamo.plazo);
 
-
 // Mostrar los resultados
+    console.log(`Monto del préstamo: $${prestamo.monto}`);
 
-    console.log(`Monto del préstamo: $${montoPrestamo}`);
+    console.log(`Tasa de interés anual: ${prestamo.tasa}%`);
 
-    console.log(`Tasa de interés anual: ${tasaInteresAnual}%`);
+    console.log(`Plazo del préstamo: ${prestamo.plazo} meses`);
 
-    console.log(`Plazo del préstamo: ${plazoMeses} meses`);
-
-    console.log(`Pago mensual: $${pagoMensual}`);
+    console.log(`Pago mensual: $${prestamo.pagoMensual}`);
 
 // Mostrar los resultados en un alert
-    alert(`Monto del préstamo: $${montoPrestamo}\nTasa de interés anual: ${tasaInteresAnual}%\nPlazo del préstamo: ${plazoMeses} meses\nPago mensual: $${pagoMensual}`);
+    alert(`Monto del préstamo: $${prestamo.monto}\nTasa de interés anual: ${prestamo.tasa}%\nPlazo del préstamo: ${prestamo.plazo} meses\nPago mensual: $${prestamo.pagoMensual}`);
 }
 
 // Mostrar los resultados en un alert después de confirmación
@@ -61,7 +56,7 @@ const confirmacion = prompt("¿Desea volver al simulador? (Sí/No)").toLowerCase
 if (confirmacion === "si" || confirmacion === "sí") {
     
    
-const alertMessage = `Monto del préstamo: $${montoPrestamo}\nTasa de interés anual: ${tasaInteresAnual}%\nPlazo del préstamo: ${plazoMeses} meses\nPago mensual: $${pagoMensual}`;
+const alertMessage = `Monto del préstamo: $${prestamo.monto}\nTasa de interés anual: ${prestamo.tasa}%\nPlazo del préstamo: ${prestamo.plazo} meses\nPago mensual: $${prestamo.pagoMensual}`;
     
     alert
 alert(alertMessage);
