abeed48fc661afa0d9346d9f5fe04fcf7e48b35c
sbeastian garcia <garciaguanco@icloud.com>
2023-08-22 21:17:45 -0300

    subo la 3ra pre entrega con las consignas solicitadas

--- js/main.js
+++ js/main.js
@@ -1,73 +1,76 @@
+// Creo un array vacio para almacenar los prestamos que voy a crear
+const prestamos = [];
+
 // Función para calcular el pago mensual del préstamo
-function calcularPagoMensual(montoPrestamo, tasaInteresAnual, plazoMeses) {
-    const tasaInteresMensual = tasaInteresAnual / (12 * 100);
+function calcularPagoMensual() {
+  // Obtenemos el value que tenemos en los inputs
+  const inputMonto = document.getElementById("monto").value;
+  const inputTasaInteresAnual = document.getElementById("tasa").value;
+  const inputPlazoMeses = document.getElementById("plazo").value;
 
-    const pagoMensual = (montoPrestamo * tasaInteresMensual) / (1 - Math.pow(1 + tasaInteresMensual, -plazoMeses));
+  // Transformamos los valores input string a numeros para hacer los calculos
+  const monto = Number(inputMonto);
+  const plazo = Number(inputPlazoMeses);
+  const tasa = Number(inputTasaInteresAnual);
 
-    return pagoMensual.toFixed(2);
-}
-calcularPagoMensual()
+  //Hacemos los calculos
+  const tasaInteresMensual = Number(inputTasaInteresAnual) / (12 * 100);
+  const pagoMensual =
+    (Number(monto) * tasaInteresMensual) /
+    (1 - Math.pow(1 + tasaInteresMensual, -Number(inputPlazoMeses)));
 
-// Crear un objeto para almacenar la información del préstamo
-const prestamo = {
-    monto: 0,
-    tasa: 0,
-    plazo: 0,
-    pagoMensual: 0
-};
+  //Creamos el objeto prestamo con los valores que tenemos pasados a numeros
+  const prestamo = { monto, plazo, tasa, pagoMensual };
 
-// Pedir al usuario los datos del préstamo y validar que sean números válidos
-prestamo.monto = parseFloat(prompt("Ingrese el monto del préstamo:"));
-prestamo.tasa = parseFloat(prompt("Ingrese la tasa de interés anual (%):"));
-prestamo.plazo = parseInt(prompt("Ingrese el plazo del préstamo en meses:"));
+  // Agregamos el prestamo al array de prestamos
+  prestamos.push(prestamo);
+  // Guardamos en localStorage los prestamos
+  localStorage.setItem("prestamos", JSON.stringify(prestamos));
 
-// Validar que los datos ingresados  y generar mensajes de validación
-const mensajesValidacion = [
-    { condicion: isNaN(prestamo.monto) || prestamo.monto <= 0, mensaje: "Por favor, ingrese un monto válido mayor que cero. Error en monto" },
-    { condicion: isNaN(prestamo.tasa) || prestamo.tasa <= 0, mensaje: "Por favor, ingrese una tasa de interés válida mayor que cero. Error en tasa" },
-    { condicion: isNaN(prestamo.plazo) || prestamo.plazo <= 0, mensaje: "Por favor, ingrese un plazo válido mayor que cero. Error en plazo" }
-];
+  // Creamos los elementos para mostar los resultados
+  const h2Monto = document.createElement("h2");
+  h2Monto.textContent = "El monto del prestamo es: " + monto;
+  const h2Tasa = document.createElement("h2");
+  h2Tasa.textContent = "El plazo del prestamo es: " + tasa;
+  const h2Plazo = document.createElement("h2");
+  h2Plazo.textContent = "La tasa del prestamo es: " + plazo;
+  const h2PagoMensual = document.createElement("h2");
+  h2PagoMensual.textContent = "El pago mensual es de: " + pagoMensual;
 
-// Filtrar mensajes de validación que sean verdaderos y mostrarlos
-const mensajesMostrar = mensajesValidacion.filter(validacion => validacion.condicion).map(validacion => validacion.mensaje);
-if (mensajesMostrar.length > 0) {
-    mensajesMostrar.forEach(mensaje => console.log(mensaje));
-} else {
+  // Creamos un titulo
+  const h2 = document.createElement("h2");
+  h2.textContent = "Resultado de mi prestamo actual";
+  document.body.appendChild(h2);
 
-// Calcular el pago mensual usando la función
-prestamo.pagoMensual = calcularPagoMensual(prestamo.monto, prestamo.tasa, prestamo.plazo);
-
-// Mostrar los resultados
-    console.log(`Monto del préstamo: $${prestamo.monto}`);
-
-    console.log(`Tasa de interés anual: ${prestamo.tasa}%`);
-
-    console.log(`Plazo del préstamo: ${prestamo.plazo} meses`);
-
-    console.log(`Pago mensual: $${prestamo.pagoMensual}`);
-
-// Mostrar los resultados en un alert
-    alert(`Monto del préstamo: $${prestamo.monto}\nTasa de interés anual: ${prestamo.tasa}%\nPlazo del préstamo: ${prestamo.plazo} meses\nPago mensual: $${prestamo.pagoMensual}`);
+  // Agregamos los resultados que creamos mas arriba
+  document.body.appendChild(h2Monto);
+  document.body.appendChild(h2Plazo);
+  document.body.appendChild(h2Tasa);
+  document.body.appendChild(h2PagoMensual);
 }
 
-// Mostrar los resultados en un alert después de confirmación
-const confirmacion = prompt("¿Desea volver al simulador? (Sí/No)").toLowerCase();
+// Creamos una funcion para ver los prestamos de localStorage
+function verPrestamosEnLocalStorage() {
 
-if (confirmacion === "si" || confirmacion === "sí") {
-    
-   
-const alertMessage = `Monto del préstamo: $${prestamo.monto}\nTasa de interés anual: ${prestamo.tasa}%\nPlazo del préstamo: ${prestamo.plazo} meses\nPago mensual: $${prestamo.pagoMensual}`;
-    
-alert(alertMessage);
-} 
+  // Obtemenmos el valor de la key prestamos de localStorage
+  const prestamosEnLocalStorage = localStorage.getItem("prestamos");
+  
+  // Creamos un titulo para los prestamos de localStorage
+  const h2 = document.createElement("h2");
+  h2.textContent = "Prestamos en localStorage";
+  document.body.appendChild(h2);
 
-else {
-    
-   
-console.log("Resultados no mostrados en el alert.");
+  //Parseamos el string a array javascript
+  //y recorremos ese array generando un h5 con el valor del monto del prestamo como texto
+  JSON.parse(prestamosEnLocalStorage).map((prestamoEnLocalStorage) => {
+    const h5 = document.createElement("h5");
+    h5.textContent =
+      "Monto del prestamo en localStorage es: " + prestamoEnLocalStorage.monto;
+    document.body.appendChild(h5);
+    return;
+  });
 }
 
 // aplicando el DOM
-document.querySelector('h1').textContent = 'La mejor Tasa del Mercado!'
-console.log(document.querySelector('h1'))
-
+document.querySelector("h1").textContent = "La mejor Tasa del Mercado!";
+console.log(document.querySelector("h1"));
